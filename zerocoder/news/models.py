00e1dc901d976bac0c35cff07ab6259e2c28f201
MamaFima <164761211+MamaFima@users.noreply.github.com>
2024-09-15 19:26:36 +0700

    Update models.py

--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User  # Импорт модели пользователя
+from django.contrib.auth.models import User  
 
 class News_post(models.Model):
     title = models.CharField('Название новости', max_length=200)
@@ -15,13 +15,12 @@ class News_post(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
 
-# Новый класс для статей
 class Article(models.Model):
     title = models.CharField('Название статьи', max_length=200)
     short_description = models.CharField('Краткое описание статьи', max_length=250)
     text = models.TextField('Текст статьи')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Привязываем автора
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
 
     def __str__(self):
         return self.title
